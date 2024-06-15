0dfb8eb7564b50536f6ac99b863c44a7d8564f29
opsline-cgomez <jcgomez16@gmail.com>
2024-06-15 02:10:44 -0500

    test

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -24,6 +24,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  depends_on = [ aws_s3_bucket_website_configuration.this ]
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.this.json
 }
