c2e7f62f0babae2e486186697da77bd4848351c9
opsline-cgomez <jcgomez16@gmail.com>
2024-06-15 01:47:32 -0500

    test

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "labs-cgomez-terraform-state-bucket"
+    key = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.27.0"
+    }
+  }
+}
---
+++ infra/terraform/main.tf
@@ -0,0 +1,87 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "${var.environment}-fsl-cgomez"
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.this.website_endpoint
+    origin_id   = aws_s3_bucket.this.bucket_regional_domain_name
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy = "http-only"
+      origin_read_timeout = 30
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "${var.environment} clodufront site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress = true
+    cache_policy_id  = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.this.bucket_regional_domain_name
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+}
